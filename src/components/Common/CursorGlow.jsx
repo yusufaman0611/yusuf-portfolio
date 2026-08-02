168dfe7b4535da293c9da346b44b201b2e7aecb4
Yusuf Aman <yusufaman75@gmail.com>
2026-08-02 10:09:42 +0530

    Delete src directory

--- src/components/Common/CursorGlow.jsx
+++
@@ -1,6 +0,0 @@
-import { useCursorGlow } from '../../hooks/useCursorGlow'
-
-export default function CursorGlow() {
-  const glowRef = useCursorGlow()
-  return <div className="cursor-glow" aria-hidden="true" ref={glowRef} />
-}
--- src/components/Common/GrainOverlay.jsx
+++
@@ -1,3 +0,0 @@
-export default function GrainOverlay() {
-  return <div className="grain-overlay" aria-hidden="true" />
-}
